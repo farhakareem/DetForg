f1b11c118c1ce4a63754e7567e42df08bdae4409
Farha Kareem <farhakareem998@gmail.com>
2020-06-17 02:34:47 +0530

    changed back

--- staticfiles/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.btn').on('click',function(){
-        $('.spinner-border').removeClass("hide");
-        //$('.btn').attr('disabled',true);
-        $('.btn-txt').text("Detecting...");
-    });
-});
